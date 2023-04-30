3feda5ffef9a8aff938280b472b8571e884a11d5
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-30 17:23:23 +0300

    create nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './Components/nav/nav.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
