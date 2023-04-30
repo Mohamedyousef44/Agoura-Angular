c3a84f2fad9c5a11b782210b458ed837a75acdee
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-30 21:08:22 +0300

    create footer-upper component

---
+++ src/app/Components/footer-upper/footer-upper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-upper',
+  templateUrl: './footer-upper.component.html',
+  styleUrls: ['./footer-upper.component.css']
+})
+export class FooterUpperComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { HomeComponent } from './Components/home/home.component';
 import { FooterColumnComponent } from './Components/footer-column/footer-column.component';
+import { FooterUpperComponent } from './components/footer-upper/footer-upper.component';
 
 
 
@@ -13,7 +14,8 @@ import { FooterColumnComponent } from './Components/footer-column/footer-column.
     AppComponent,
     NavBarComponent,
     HomeComponent,
-    FooterColumnComponent
+    FooterColumnComponent,
+    FooterUpperComponent
   ],
   imports: [
     BrowserModule,
