7ea9e292433cbd3a4b77f76975a51899e42ae99c
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-30 21:36:09 +0300

    create footer-lower component

---
+++ src/app/Components/footer-lower/footer-lower.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer-lower',
+  templateUrl: './footer-lower.component.html',
+  styleUrls: ['./footer-lower.component.css']
+})
+export class FooterLowerComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { HomeComponent } from './Components/home/home.component';
 import { FooterColumnComponent } from './Components/footer-column/footer-column.component';
 import { FooterUpperComponent } from './Components/footer-upper/footer-upper.component';
+import { FooterLowerComponent } from './components/footer-lower/footer-lower.component';
 
 
 
@@ -16,7 +17,8 @@ import { FooterUpperComponent } from './Components/footer-upper/footer-upper.com
     NavBarComponent,
     HomeComponent,
     FooterColumnComponent,
-    FooterUpperComponent
+    FooterUpperComponent,
+    FooterLowerComponent
   ],
   imports: [
     BrowserModule,
