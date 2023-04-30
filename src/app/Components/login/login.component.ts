d3a8e131d0273f27c4a7203a1ee438d0f4c71be0
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-04-30 23:43:22 +0300

    init login page

---
+++ src/app/Components/login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styles: [
+  ]
+})
+export class LoginComponent {
+
+}
