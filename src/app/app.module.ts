7d0ab76219777d49488eaac180c049a8fc3f2c37
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-30 21:35:15 +0300

    create html and css footer-upper component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { HomeComponent } from './Components/home/home.component';
 import { FooterColumnComponent } from './Components/footer-column/footer-column.component';
-import { FooterUpperComponent } from './components/footer-upper/footer-upper.component';
+import { FooterUpperComponent } from './Components/footer-upper/footer-upper.component';
+
 
 
 
