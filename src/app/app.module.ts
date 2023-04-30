03cd493d2cbd1c3d0439da20e4c49099bd0abcb7
omaralaa <omaralaa0989@gmail.com>
2023-04-30 18:00:09 +0300

    IMPORT HTTPCLIENTMODULE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './Components/nav/nav.component';
 import { MainPageItemsSectionComponent } from './MainPageComponent/main-page-items-section/main-page-items-section.component';
-
+import{HttpClientModule}from'@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,9 @@ import { MainPageItemsSectionComponent } from './MainPageComponent/main-page-ite
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
