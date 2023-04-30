de9a673668bddd6fd2dd7307e7ddb4eb49d44942
omaralaa <omaralaa0989@gmail.com>
2023-04-30 12:46:26 +0300

    create MainPageItemsSection Component

---
+++ src/app/MainPageComponent/main-page-items-section/main-page-items-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page-items-section',
+  templateUrl: './main-page-items-section.component.html',
+  styleUrls: ['./main-page-items-section.component.css']
+})
+export class MainPageItemsSectionComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './Components/nav/nav.component';
+import { MainPageItemsSectionComponent } from './MainPageComponent/main-page-items-section/main-page-items-section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    MainPageItemsSectionComponent
   ],
   imports: [
     BrowserModule,
