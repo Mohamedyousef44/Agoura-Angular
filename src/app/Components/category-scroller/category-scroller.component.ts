5716059de2454838460b96715331e01a6f17fe64
Ahmed Hamada <ahmedhamada.labs@gmail.com>
2023-05-01 00:38:54 +0300

    add scroller functionality

--- src/app/Components/category-scroller/category-scroller.component.ts
+++ src/app/Components/category-scroller/category-scroller.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-category-scroller',
   templateUrl: './category-scroller.component.html',
-  styleUrls: ['./category-scroller.component.css']
+  styleUrls: ['./category-scroller.component.css'],
 })
 export class CategoryScrollerComponent {
-
+  scroll(scroller: any, degree: number) {
+    scroller.scrollLeft += degree;
+  }
 }
