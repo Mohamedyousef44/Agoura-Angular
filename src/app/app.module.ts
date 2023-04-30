9b74c9ce27819142f95a9fbcdbe8369b0af8d00d
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-30 19:44:32 +0300

    add final colors and img background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { AHoverDirective } from './directives/a-hover.directive';
+import { HomeComponent } from './components/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    AHoverDirective
+    AHoverDirective,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
