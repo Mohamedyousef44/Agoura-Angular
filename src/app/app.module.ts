ec763b48b866f0be5e5a715fa1a3f4485d476dc5
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-04-30 18:21:07 +0300

    create hover directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { AHoverDirective } from './directives/a-hover.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    AHoverDirective
   ],
   imports: [
     BrowserModule,
