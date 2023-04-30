13e8aba0ee46bbc28f938beb351e074d932640db
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-05-01 01:31:31 +0300

    login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './Components/nav/nav.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes : Routes = [
+  {path : 'login' , component : LoginComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
