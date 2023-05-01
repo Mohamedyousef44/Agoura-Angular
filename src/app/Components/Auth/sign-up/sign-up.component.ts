74bf750b6e47574ebac7235bcec976632700df9b
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-05-01 17:33:58 +0300

    SignUp page

---
+++ src/app/Components/Auth/sign-up/sign-up.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styles: [
+  ]
+})
+export class SignUpComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './Components/nav/nav.component';
-import { LoginComponent } from './Components/login/login.component';
+import { LoginComponent } from './Components/Auth/login/login.component';
 import { RouterModule, Routes } from '@angular/router';
+import { FacebookButtonComponent } from './Components/Auth/thirdPartyLoginButtons/facebook-button/facebook-button.component';
+import { GoogleButtonComponent } from './Components/Auth/thirdPartyLoginButtons/google-button/google-button.component';
+import { AppleButtonComponent } from './Components/Auth/thirdPartyLoginButtons/apple-button/apple-button.component';
+import { SignUpComponent } from './Components/Auth/sign-up/sign-up.component';
 
 const routes : Routes = [
-  {path : 'login' , component : LoginComponent}
+  {path : 'login' , component : LoginComponent},
+  {path : 'signup' , component: SignUpComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    LoginComponent
+    LoginComponent,
+    FacebookButtonComponent,
+    GoogleButtonComponent,
+    AppleButtonComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
