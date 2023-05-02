64649af405e89059a06dba6531c65edf777f0a19
Ahmed Hamada <ahmedhamada.labs@gmail.com>
2023-05-02 18:02:57 +0300

    add product info

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,22 +5,26 @@ import { HomeComponent } from './Components/home/home.component';
 import { LayoutComponent } from './Components/layout/layout.component';
 import { LoginComponent } from './Components/Auth/login/login.component';
 import { SignUpComponent } from './Components/Auth/sign-up/sign-up.component';
+import { BidHistoryComponent } from './Components/bid-history/bid-history.component';
 
 const routes: Routes = [
-
-  {path: "",redirectTo: '/home' , pathMatch: 'full' },
-  {path: "",component:LayoutComponent,children:[
-    {path:"productdetails",component:ProductDetailsComponent}
-  ]
-},
-  {path : "home",component:HomeComponent},
-  {path : 'login' , component : LoginComponent},
-  {path : 'signup' , component: SignUpComponent}
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: 'productdetails', component: ProductDetailsComponent },
+      { path: 'bidhistory', component: BidHistoryComponent },
+    ],
+  },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent },
   // {path : "**",}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
