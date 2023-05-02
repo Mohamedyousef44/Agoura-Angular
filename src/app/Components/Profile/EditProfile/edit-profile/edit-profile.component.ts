0b92e3b8d2315ee61c1af2a3b93c5f1a17b5c0cc
omaralaa <omaralaa0989@gmail.com>
2023-05-03 01:16:22 +0300

    add constructor and inject service

--- src/app/Components/Profile/EditProfile/edit-profile/edit-profile.component.ts
+++ src/app/Components/Profile/EditProfile/edit-profile/edit-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MainPageService } from 'src/app/Service/main-page.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent {
+  ID:any
+  constructor(myRoute: ActivatedRoute, public myService:MainPageService,public router:Router) {
+    this.ID = myRoute.snapshot.params['id'];
+  }
 
 }
