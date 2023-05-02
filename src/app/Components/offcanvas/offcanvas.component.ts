10769943f7d631b0e673d436d7a66f8f43cfdc4b
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-02 14:41:57 +0300

    create offcanvas component

---
+++ src/app/Components/offcanvas/offcanvas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offcanvas',
+  templateUrl: './offcanvas.component.html',
+  styleUrls: ['./offcanvas.component.css']
+})
+export class OffcanvasComponent {
+
+}
