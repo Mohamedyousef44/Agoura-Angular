8ad2126dc3ca5bda9a8465f5ab22b488e2a19cdc
Ahmed Hamada <ahmedhamada.labs@gmail.com>
2023-05-02 17:30:22 +0300

    add bid history component

---
+++ src/app/Components/bid-history/bid-history.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bid-history',
+  templateUrl: './bid-history.component.html',
+  styleUrls: ['./bid-history.component.css']
+})
+export class BidHistoryComponent {
+
+}
