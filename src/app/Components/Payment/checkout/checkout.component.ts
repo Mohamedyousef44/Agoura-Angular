33ca2c13f526f32aa926ddbd61007e6552a5aea5
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-05-03 22:38:00 +0300

    Nav for the checkout page

---
+++ src/app/Components/Payment/checkout/checkout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styles: [
+  ]
+})
+export class CheckoutComponent {
+
+}
