ac9bd0a1459dd3c3ff5620799fdaee25190f2d7d
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-05-03 22:38:57 +0300

    Shipping-address

---
+++ src/app/Components/Payment/shipping-address/shipping-address.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping-address',
+  templateUrl: './shipping-address.component.html',
+  styles: [
+  ]
+})
+export class ShippingAddressComponent {
+
+}
