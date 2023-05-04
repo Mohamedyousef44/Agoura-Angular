6f4f6f59faf9e31bb1242802f8018ceacd0fdf9b
omaralaa <omaralaa0989@gmail.com>
2023-05-04 13:48:08 +0300

    add updateUser function

--- src/app/Components/Profile/EditProfile/edit-profile/edit-profile.component.ts
+++ src/app/Components/Profile/EditProfile/edit-profile/edit-profile.component.ts
@@ -11,7 +11,6 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class EditProfileComponent implements OnInit {
   ID = 1;
   User: any;
-  UserNewData: any;
   validationForm:any
 
   //get user id from route
@@ -22,6 +21,8 @@ export class EditProfileComponent implements OnInit {
   ) {
     // this.ID = myRoute.snapshot.params['id'];
   }
+
+
 
   //get the user from db to remeber the data
   ngOnInit(): void {
@@ -36,27 +37,17 @@ export class EditProfileComponent implements OnInit {
             phone: new FormControl(null, [Validators.required]),
             email: new FormControl(null, [Validators.email, Validators.required]),
             profileImage: new FormControl(null, [ Validators.pattern(/\.(jpe?g|webp|png)$/i),]),
-            password: new FormControl(null,[ Validators.required, Validators.minLength(10), Validators.maxLength(60) ])
+            // password: new FormControl(null,[  Validators.minLength(10), Validators.maxLength(60) ])
           });
         this.validationForm.patchValue(this.User)
-        //  this.validationForm = new FormGroup({
-        //   userName: new FormControl(this.User.userName, [Validators.required,Validators.minLength(3) ]),
-        //   age: new FormControl(null, [Validators.required]),
-        //   address: new FormControl(null, [Validators.required]),
-        //   phone: new FormControl(null, [Validators.required]),
-        //   email: new FormControl(null, [Validators.email, Validators.required]),
-        //   profileImage: new FormControl(null, [ Validators.pattern(/\.(jpe?g|webp|png)$/i),]),
-        // });
       },
       error: (err) => {},
     });
   }
 
-  //validate user inputs in update
-  ///////////////////
-//<div class="invalid-feedback" *ngIf="title.dirty && !title.valid">
-// please provide a good title.
-// </div>
+
+
+
 
   get userName() {
     return this.validationForm.controls['userName'];
@@ -81,66 +72,16 @@ export class EditProfileComponent implements OnInit {
     return this.validationForm.controls["password"];
   }
 
-  // get ageValid() {
-  //   return this.validationForm.controls['age'].valid;
-  // }
-  // get nameValid() {
-  //   return this.validationForm.controls['userName'].valid;
-  // }
 
-  // get emailValid() {
-  //   return this.validationForm.controls['email'].valid;
-  // }
-  // get addressValid() {
-  //   return this.validationForm.controls['address'].valid;
-  // }
-  // get phoneValid() {
-  //   return this.validationForm.controls['phone'].valid;
-  // }
 
-  // get profileImageValid() {
-  //   return this.validationForm.controls['profileImage'].valid;
-  // }
 
-//   updateValidatedData(
-//     newname: any,
-//     newemail: any,
-//     newage: any,
-//     newprofileImage: any,
-//     newadress: any,
-//     newAbout: any,
-//     newphone: any
-//   ) {
-//     if (this.validationForm.valid) {
-//       this.UserNewData = {
-//         userName: newname,
-//         age: newage,
-//         email: newemail,
-//         profileImage: newprofileImage,
-//         address: newadress,
-//         About: newAbout,
-//         phone: newphone,
-//       };
-//       this.updateUserDataInDataBase()
-//     }else{alert("wrong")}
-
-//   }
-
-// updateUserDataInDataBase(){
-
-// this.myService.UpdateUser(this.UserNewData,this.ID).subscribe(data => {
-//   console.log(data);})
-//   this.router.navigateByUrl('/students');
-
-// }
-// }
-// update(){
-//   if (this.validationForm.valid){
-//     this.myService.UpdateUser(this.validationForm.value,this.ID).subscribe(data => {
-      //   console.log(data);})
-      //   this.router.navigateByUrl('/students');
-//   }
-// }
+updateUser(){
+  if (this.validationForm.valid){
+    this.myService.UpdateUser(this.validationForm.value,this.ID).subscribe(data => {
+        console.log(data);})
+        this.router.navigateByUrl('/students');
+  }
+}
 
 
 
