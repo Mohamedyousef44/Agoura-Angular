355e1f39e40875008df6f72326c6d7d93d729ee2
omaralaa <omaralaa0989@gmail.com>
2023-05-04 08:37:27 +0300

    change update user form layout

--- src/app/Components/Profile/EditProfile/edit-profile/edit-profile.component.ts
+++ src/app/Components/Profile/EditProfile/edit-profile/edit-profile.component.ts
@@ -12,6 +12,7 @@ export class EditProfileComponent implements OnInit {
   ID = 1;
   User: any;
   UserNewData: any;
+  validationForm:any
 
   //get user id from route
   constructor(
@@ -28,100 +29,123 @@ export class EditProfileComponent implements OnInit {
       next: (data) => {
         console.log(data);
         this.User = data;
+        this.validationForm = new FormGroup({
+            userName: new FormControl(null, [Validators.required,Validators.minLength(3) ]),
+            age: new FormControl(null, [Validators.required]),
+            address: new FormControl(null, [Validators.required]),
+            phone: new FormControl(null, [Validators.required]),
+            email: new FormControl(null, [Validators.email, Validators.required]),
+            profileImage: new FormControl(null, [ Validators.pattern(/\.(jpe?g|webp|png)$/i),]),
+          });
+        this.validationForm.patchValue(this.User)
+        //  this.validationForm = new FormGroup({
+        //   userName: new FormControl(this.User.userName, [Validators.required,Validators.minLength(3) ]),
+        //   age: new FormControl(null, [Validators.required]),
+        //   address: new FormControl(null, [Validators.required]),
+        //   phone: new FormControl(null, [Validators.required]),
+        //   email: new FormControl(null, [Validators.email, Validators.required]),
+        //   profileImage: new FormControl(null, [ Validators.pattern(/\.(jpe?g|webp|png)$/i),]),
+        // });
       },
       error: (err) => {},
     });
   }
 
   //validate user inputs in update
-  validationForm = new FormGroup({
-    userName: new FormControl(null, [
-      Validators.required,
-      Validators.minLength(3),
-    ]),
-    age: new FormControl(null, [
-      Validators.min(20),
-      Validators.max(40),
-      Validators.required,
-    ]),
-    address: new FormControl(null, Validators.required),
-    phone: new FormControl(null, Validators.required),
-    email: new FormControl(null, [Validators.email, Validators.required]),
-    profileImage: new FormControl(null, [
-      Validators.pattern(/\.(jpe?g|webp)$/i),
-    ]),
-  });
+  ///////////////////
+//<div class="invalid-feedback" *ngIf="title.dirty && !title.valid">
+// please provide a good title.
+// </div>
 
-  get name() {
-    return this.validationForm.controls['userName'].value;
+  get userName() {
+    return this.validationForm.controls['userName'];
   }
   get age() {
-    return this.validationForm.controls['age'].value;
+    return this.validationForm.controls['age'];
   }
   get email() {
-    return this.validationForm.controls['email'].value;
+    return this.validationForm.controls['email'];
   }
   get phone() {
-    return this.validationForm.controls['phone'].value;
+    return this.validationForm.controls['phone'];
   }
   get address() {
-    return this.validationForm.controls['address'].value;
+    return this.validationForm.controls['address'];
   }
 
   get profileImage() {
-    return this.validationForm.controls['profileImage'].value;
+    return this.validationForm.controls['profileImage'];
   }
 
-  get AgeValid() {
-    return this.validationForm.controls['age'].valid;
-  }
-  get nameValid() {
-    return this.validationForm.controls['userName'].valid;
-  }
+  // get ageValid() {
+  //   return this.validationForm.controls['age'].valid;
+  // }
+  // get nameValid() {
+  //   return this.validationForm.controls['userName'].valid;
+  // }
 
-  get emailValid() {
-    return this.validationForm.controls['email'].valid;
-  }
-  get addressValid() {
-    return this.validationForm.controls['address'].valid;
-  }
-  get phoneValid() {
-    return this.validationForm.controls['phone'].valid;
-  }
+  // get emailValid() {
+  //   return this.validationForm.controls['email'].valid;
+  // }
+  // get addressValid() {
+  //   return this.validationForm.controls['address'].valid;
+  // }
+  // get phoneValid() {
+  //   return this.validationForm.controls['phone'].valid;
+  // }
 
-  get profileImageValid() {
-    return this.validationForm.controls['profileImage'].valid;
-  }
+  // get profileImageValid() {
+  //   return this.validationForm.controls['profileImage'].valid;
+  // }
 
-  updateValidatedData(
-    newname: any,
-    newemail: any,
-    newage: any,
-    newprofileImage: any,
-    newadress: any,
-    newAbout: any,
-    newphone: any
-  ) {
-    if (this.validationForm.valid) {
-      this.UserNewData = {
-        userName: newname,
-        age: newage,
-        email: newemail,
-        profileImage: newprofileImage,
-        address: newadress,
-        About: newAbout,
-        phone: newphone,
-      };
-      this.updateUserDataInDataBase()
-    }
+//   updateValidatedData(
+//     newname: any,
+//     newemail: any,
+//     newage: any,
+//     newprofileImage: any,
+//     newadress: any,
+//     newAbout: any,
+//     newphone: any
+//   ) {
+//     if (this.validationForm.valid) {
+//       this.UserNewData = {
+//         userName: newname,
+//         age: newage,
+//         email: newemail,
+//         profileImage: newprofileImage,
+//         address: newadress,
+//         About: newAbout,
+//         phone: newphone,
+//       };
+//       this.updateUserDataInDataBase()
+//     }else{alert("wrong")}
 
-  }
+//   }
 
-updateUserDataInDataBase(){
+// updateUserDataInDataBase(){
 
-this.myService.UpdateUser(this.UserNewData,this.ID).subscribe(data => {
-  console.log(data);})
-  this.router.navigateByUrl('/students');
+// this.myService.UpdateUser(this.UserNewData,this.ID).subscribe(data => {
+//   console.log(data);})
+//   this.router.navigateByUrl('/students');
 
-}
+// }
+// }
+// update(){
+//   if (this.validationForm.valid){
+//     this.myService.UpdateUser(this.validationForm.value,this.ID).subscribe(data => {
+//       //   console.log(data);})
+//       //   this.router.navigateByUrl('/students');
+//   }
+// }
+
+/**<div class="col-md-3">
+        <label for="validationServer02" class="form-label">Country</label>
+        <input type="text" formControlName="country" name="country" placeholder="country" class="form-control is-invalid" [ngClass]="{'is-valid':country!.dirty && country!.valid,'is-invalid':country!.dirty && !country!.valid}" id="validationServer02" aria-describedby="validationServer02Feedback" required>
+        <div id="validationServer02Feedback" class="invalid-feedback" *ngIf="country!.dirty &&
+!country!.valid">
+          Please provide a valid Country.
+        </div>
+      </div> */
+
+
 }
