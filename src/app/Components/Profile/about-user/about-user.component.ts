376898cfed9ea9145a90d83c9f96eecde4b2fdeb
omaralaa <omaralaa0989@gmail.com>
2023-05-04 21:28:00 +0300

    add profile router in app.routes

--- src/app/Components/Profile/about-user/about-user.component.ts
+++ src/app/Components/Profile/about-user/about-user.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MainPageService } from 'src/app/Service/main-page.service';
 
 @Component({
   selector: 'app-about-user',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-user.component.css']
 })
 export class AboutUserComponent {
+  ID=1;
+  UserDetails:any
 
+  constructor(public myService:MainPageService,myRoute:ActivatedRoute){
+    //
+    // this.ID = myRoute.snapshot.params["id"];-->active this code when data static
+  }
+
+  ngOnInit(): void {
+    this.myService.GetUserByID(this.ID).subscribe(
+      {
+        next:(data: any)=>{
+          console.log(data)
+          this.UserDetails = data;
+          console.log(data)
+        },
+        error:(err: any)=>{console.log(err)}
+      }
+    )
+  }
 }
+
+
+
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,20 @@ import { HomeComponent } from './Components/home/home.component';
 import { LayoutComponent } from './Components/layout/layout.component';
 import { LoginComponent } from './Components/Auth/login/login.component';
 import { SignUpComponent } from './Components/Auth/sign-up/sign-up.component';
+import { ProfileComponent } from './Components/Profile/profile/profile.component';
+import { EditProfileComponent } from './Components/Profile/EditProfile/edit-profile/edit-profile.component';
+import { AboutUserComponent } from './Components/Profile/about-user/about-user.component';
 
 const routes: Routes = [
 
   {path: "",redirectTo: '/home' , pathMatch: 'full' },
   {path: "",component:LayoutComponent,children:[
-    {path:"productdetails",component:ProductDetailsComponent}
+    {path:"productdetails",component:ProductDetailsComponent},
+    {path:"user/:id",component:ProfileComponent,children:[
+      // {path:"",redirectTo: '/user/:id/about' , pathMatch: 'full'},
+{path:"edit",component:EditProfileComponent},
+{path:"about",component:AboutUserComponent}
+    ]}
   ]
 },
   {path : "home",component:HomeComponent},
