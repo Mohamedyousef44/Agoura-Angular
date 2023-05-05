9138c9826432d8629b8489cf1afff846ea54d994
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-05-06 00:05:57 +0300

    move components to components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { FacebookButtonComponent } from './Components/Auth/thirdPartyLoginButton
 import { GoogleButtonComponent } from './Components/Auth/thirdPartyLoginButtons/google-button/google-button.component';
 import { AppleButtonComponent } from './Components/Auth/thirdPartyLoginButtons/apple-button/apple-button.component';
 import { SignUpComponent } from './Components/Auth/sign-up/sign-up.component';
-import { MainPageItemsSectionComponent } from './Components/main-page-items-section/main-page-items-section.component';
 import { HttpClientModule }from'@angular/common/http';
 import { CarouselModule } from 'ngx-bootstrap/carousel';
 import { CategoryScrollerComponent } from './Components/category-scroller/category-scroller.component';
@@ -23,7 +22,6 @@ import { LayoutComponent } from './Components/layout/layout.component';
 import { CheckoutComponent } from './Components/Payment/checkout/checkout.component';
 import { PaymentMethodComponent } from './Components/Payment/payment-method/payment-method.component';
 import { ProfileComponent } from './Components/Profile/profile/profile.component';
-import { EditProfileComponent } from './Components/Profile/edit-profile/edit-profile.component';
 import { AboutUserComponent } from './Components/Profile/about-user/about-user.component';
 import { AboutComponent } from './Components/about/about.component';
 import { CreateProductFormComponent } from './Components/create-product-form/create-product-form.component';
@@ -35,6 +33,8 @@ import { ForgetPasswordComponent } from './Components/forget-password/forget-pas
 import { OtpComponent } from './Components/otp/otp.component';
 import { UpToTopComponent } from './Components/up-to-top/up-to-top.component';
 import { NotfoundPageComponent } from './Components/notfound-page/notfound-page.component';
+import { EditProfileComponent } from './Components/Profile/edit-profile/edit-profile.component';
+import { MainPageItemsSectionComponent } from './Components/main-page-items-section/main-page-items-section.component';
 
 
 @NgModule({
