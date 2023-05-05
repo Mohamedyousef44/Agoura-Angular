2612db8bd9fb7f20320b39b2e04741c9a4b70a6e
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-05-05 17:57:41 +0300

    Refactor the cvv validation

--- src/app/Components/Payment/payment-method/payment-method.component.ts
+++ src/app/Components/Payment/payment-method/payment-method.component.ts
@@ -8,16 +8,20 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   ]
 })
 export class PaymentMethodComponent {
+  public customerPaymentInfo : any = {} ;
   public validationForm = new FormGroup({
     name: new FormControl(null,[Validators.required,  Validators.pattern('^[a-zA-Z ]*$'), Validators.maxLength(32) , Validators.minLength(20)]),
-    cardNumber : new FormControl(null,[Validators.required , Validators.maxLength(16) , Validators.minLength(16)]),
+    cardNumber : new FormControl(null,[Validators.required , Validators.minLength(16)]),
     expiryDate: new FormControl(null, [Validators.required, Validators.pattern('^(0[1-9]|1[0-2])\/([0-9]{4}|[0-9]{2})$') , Validators.minLength(5) ] ),
-    cvv : new FormControl(null , [Validators.required, Validators.minLength(3), Validators.maxLength(3)])
+    cvv : new FormControl(null , [Validators.required, Validators.minLength(3)])
   });
 
   validate(){
-    if (this.validationForm.valid){
-      // Hit an end-point with this info home-boy       
+    if (this.validationForm.valid){      
+      this.customerPaymentInfo["name"] = this.validationForm.controls["name"].value;
+      this.customerPaymentInfo["cardNumber"] = this.validationForm.controls["cardNumber"].value;
+      this.customerPaymentInfo["expiryDate"] = this.validationForm.controls["expiryDate"].value;
+      this.customerPaymentInfo["cvv"] = this.validationForm.controls["cvv"].value;
     }
   }
   get name () {
