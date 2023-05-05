02e3de561abe1dd3be73b4d602ea8e3a6a396883
AhmedMohamedZein <ahmedzain503@gmail.com>
2023-05-05 17:39:31 +0300

    Validation payment

--- src/app/Components/Payment/payment-method/payment-method.component.ts
+++ src/app/Components/Payment/payment-method/payment-method.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payment-method',
@@ -7,5 +8,41 @@ import { Component } from '@angular/core';
   ]
 })
 export class PaymentMethodComponent {
+  public validationForm = new FormGroup({
+    name: new FormControl(null,[Validators.required,  Validators.pattern('^[a-zA-Z ]*$'), Validators.maxLength(32) , Validators.minLength(20)]),
+    cardNumber : new FormControl(null,[Validators.required , Validators.maxLength(16) , Validators.minLength(16)]),
+    expiryDate: new FormControl(null, [Validators.required, Validators.pattern('^(0[1-9]|1[0-2])\/([0-9]{4}|[0-9]{2})$') , Validators.minLength(5) ] ),
+    cvv : new FormControl(null , [Validators.required, Validators.minLength(3), Validators.maxLength(3)])
+  });
 
+  validate(){
+    if (this.validationForm.valid){
+      // Hit an end-point with this info home-boy       
+    }
+  }
+  get name () {
+    return this.validationForm.controls["name"].valid;
+  }
+  get cardNumber () {
+    return this.validationForm.controls["cardNumber"].valid;
+  }
+  get expiryDate () {
+    return this.validationForm.controls["expiryDate"].valid;
+  }  
+  get cvv () {
+    return this.validationForm.controls["cvv"].valid
+  }
+  isNameDirty(){
+    return this.validationForm.controls["name"].dirty;
+  }
+  isCardNumberDirty(){
+    return this.validationForm.controls["cardNumber"].dirty;
+  }
+  isCVVDirty() {
+    return this.validationForm.controls["cvv"].dirty;
+  }
+
+  isExpiryDateDirty(){
+    return this.validationForm.controls["expiryDate"].dirty;
+  } 
 }
