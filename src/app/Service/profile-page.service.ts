881f7d45dd20f9a46e70d43a230cec6108f765bd
omaralaa <omaralaa0989@gmail.com>
2023-05-05 19:30:49 +0300

    inject httpclient in profile service and add getuserbyid and update user functions

--- src/app/Service/profile-page.service.ts
+++ src/app/Service/profile-page.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfilePageService {
+  Base_URL: any
 
-  constructor() { }
+  constructor(private readonly myClient:HttpClient) { }
+
+
+  GetUserByID(id:any){
+    return this.myClient.get(this.Base_URL+"/"+id);
+  }
+
+  UpdateUser(newUser:any,id:any){
+
+    return this.myClient.put(this.Base_URL+"/"+id,newUser)
+
+  }
 }
