e1c3cbbb2223e6bef9516e2f311886e3430a13b3
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-10 09:16:44 +0300

    update off canvvas ts to retrieve data from homeservice not from parent layout

--- src/app/Components/offcanvas/offcanvas.component.ts
+++ src/app/Components/offcanvas/offcanvas.component.ts
@@ -8,11 +8,29 @@ import { UserHomeDataService } from 'src/app/Service/user-home-data.service';
   styleUrls: ['./offcanvas.component.css']
 })
 export class OffcanvasComponent {
+  notificationData: any;
+  cartData: any;
 
   constructor(private service: UserHomeDataService , private route: Router){}
 
   @Input('data') data: any
   @Input('isCart') toggle: any
+
+
+
+  ngOnInit() {
+    this.service.cartUpdated.subscribe((res) => {
+      this.cartData = res.apartments;
+    });
+    this.service.getData().subscribe({
+      next:(data: any)=>{
+        this.notificationData = data["notifications"]
+        this.cartData = data["carts"][0].apartments
+        console.log(this.cartData)
+      } ,
+      error:(e:Error)=> console.log(e)
+    })
+  }
 
   deleteItem(id: any){
 
