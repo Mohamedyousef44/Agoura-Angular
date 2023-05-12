b13b01b458d033a6b0fb07336d53d36a962e776f
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-05-13 00:17:09 +0300

    refactor status codes and redirect to 404

--- src/app/Components/product-details/product-details.component.ts
+++ src/app/Components/product-details/product-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl,FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BidsService } from 'src/app/Service/bids.service';
 
 @Component({
@@ -14,42 +14,34 @@ export class ProductDetailsComponent implements OnInit{
   placeObj!:any;
   maxBid!:number;
   thumbnail!:any;
-  public constructor(private myService:BidsService,private route:ActivatedRoute){
-    // this.maxBid=0;
-    // this.form=new FormGroup({
-    //   bid: new FormControl(this.maxBid,Validators.min(this.maxBid+1)),
-    // })
+  public constructor(private myService:BidsService,private route:ActivatedRoute,private router:Router){
+    
   }
   ngOnInit(): void {
     let bidId=this.route.snapshot.params["id"]
-    this.myService.GetBidById(bidId).subscribe(
-      {
+    this.myService.GetBidById(bidId).subscribe({
       next:(res:any)=>{
-        // this.populate(data)
-        this.placeObj=res.data.appartment
-        this.maxBid=res.data.appartment.bids[0].amountMoney
-        this.thumbnail=res.data.appartment.images[0]
-        this.form=new FormGroup({
-          bid: new FormControl(this.maxBid,Validators.min(this.maxBid+1)),
-        })
-        console.log(res.data.appartment)
-        },
-        error:(err)=>{  
-          console.log(err)
+        console.log(res)
+        if(res.success){
+          this.populate(res)
+          console.log(res.data)
         }
+      },
+      error:(err)=>{  
+        this.router.navigateByUrl(`place/${bidId}/notfound`)
+        console.log(err)
       }
-    )
+    })
     
   }
 
-  public populate(data:any){
-    this.placeObj=data
-    this.maxBid=data.bids.amountMoney
-    this.thumbnail=data.images[0]
-    // this.form=new FormGroup({
-    //   bid: new FormControl(this.maxBid,Validators.min(this.maxBid+1)),
-    // })
-    console.log(data)
+  public populate(res:any){
+    this.placeObj=res.data.appartment
+    this.maxBid=res.data.appartment.bids[0].amountMoney
+    this.thumbnail=res.data.appartment.images[0]
+    this.form=new FormGroup({
+      bid: new FormControl(this.maxBid,Validators.min(this.maxBid+1)),
+    })
   }
   
   public changeImage(event:any){
@@ -60,10 +52,29 @@ export class ProductDetailsComponent implements OnInit{
   }
 
   onSubmit(){
-    if(!this.form.valid){
+    if(!this.form.valid || !localStorage.getItem("X-Auth-Token")){
       return
     }
     // toDo add form submit logic here
+    let data={
+      amountMoney:this.bid!.value,
+      apartmentID:this.placeObj._id,
+      userToken:localStorage.getItem("X-Auth-Token")
+    }
+    console.log(data)
+    this.myService.addNewBid(data).subscribe(
+      {
+        next:(res:any)=>{
+          if(res.success){
+            this.populate(res)
+            console.log(res.data)
+          }
+          },
+          error:(err)=>{  
+            console.log(err)
+          }
+        }
+    );
     console.log("form submitted",this.form.valid)
 
   }
