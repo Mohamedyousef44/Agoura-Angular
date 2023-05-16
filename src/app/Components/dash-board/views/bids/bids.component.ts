dfff392bce3f6725eebd78c47c2f0fb8befe3504
omaralaa <omaralaa0989@gmail.com>
2023-05-16 12:49:42 +0300

    retrieve data and show it in table

--- src/app/Components/dash-board/views/bids/bids.component.ts
+++ src/app/Components/dash-board/views/bids/bids.component.ts
@@ -7,12 +7,14 @@ import { DashboardTableService } from 'src/app/Service/dashboard-table.service';
   styleUrls: ['./bids.component.css']
 })
 export class BidsComponent {
+  allAppartments:any
   constructor(public myService:DashboardTableService){}
   ngOnInit(): void {
     this.myService.GetAllAppartment().subscribe(
       {
         next:(data: any)=>{
-        console.log(data);
+        console.log(data[1]);
+        this.allAppartments=data;
         },
         error:(err: any)=>{console.log(err)}
       }
