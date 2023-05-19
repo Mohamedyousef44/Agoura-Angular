12f851e7280610542fd77d18bde27a9a5b569205
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-05-20 00:03:05 +0300

    intercept http requests to show toast

--- src/app/intercreptors/response.interceptor.ts
+++ src/app/intercreptors/response.interceptor.ts
@@ -3,34 +3,48 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpResponse,
+  HttpErrorResponse,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { Router } from '@angular/router';
-
+import { ToastService } from '../Service/toast-service.service';
 
 @Injectable()
 export class ResponseInterceptor implements HttpInterceptor {
-
-  constructor(private route: Router) {}
+  constructor(private route: Router, private toastService: ToastService) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
-      tap((event) => {
-        if (event.type === 4) { // Check if event is an HttpResponse
-          if(!event.ok){
-            this.route.navigateByUrl('**')
+      tap({
+        next: (event) => {
+          if (event.type === 4) {
+            // Check if event is an HttpResponse
+            if (event.status > 200 && event instanceof HttpResponse) {
+              this.toastService.activateToast(
+                event.body.message,
+                event.body.success
+              );
+            }
           }
-          console.log(event.ok)
-          console.log('Received response:', event);
-        }
+        },
+        error: (event: HttpErrorResponse) => {
+          if (event.status == 404) {
+            this.route.navigateByUrl('404-notfound');
+          }
+          if (event instanceof HttpErrorResponse) {
+            this.toastService.activateToast(
+              event.error.message,
+              event.error.success
+            );
+          }
+        },
       })
     );
   }
 }
-
-
