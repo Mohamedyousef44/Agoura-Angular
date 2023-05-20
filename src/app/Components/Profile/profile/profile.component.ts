ad86714279bb1cfca1f5d0f9842e38f205b7e81a
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-05-21 00:30:12 +0300

    bind change user picture with backend

--- src/app/Components/Profile/profile/profile.component.ts
+++ src/app/Components/Profile/profile/profile.component.ts
@@ -4,88 +4,87 @@ import { ActivatedRoute } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ProfilePageService } from 'src/app/Service/profile-page.service';
 
-
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
 export class ProfileComponent implements OnInit {
-
-  userID: any
-  UserDetails:any
-  userImage: any
-  validationForm: any
+  userID: any;
+  UserDetails: any;
+  userImage: any;
+  validationForm: any;
   profileValid = false;
 
-  constructor(public myService:ProfilePageService , public route: ActivatedRoute , private spinner: NgxSpinnerService ){
-    this.route.params.subscribe(params => {
+  constructor(
+    public myService: ProfilePageService,
+    public route: ActivatedRoute,
+    private spinner: NgxSpinnerService
+  ) {
+    this.route.params.subscribe((params) => {
       this.userID = params['id'];
-    })
+    });
   }
 
-  goDown(){
-    window.scroll(0 , 630)
+  goDown() {
+    window.scroll(0, 630);
   }
 
   ngOnInit(): void {
+    this.spinner.show('imageSpinner');
+    this.myService.Image.subscribe((info) => {
+      this.userImage = info.data.image;
+    });
+    this.spinner.hide('imageSpinner');
+    this.spinner.show('profileSpinner');
+    this.myService.GetUserByID(this.userID).subscribe({
+      next: (data: any) => {
+        this.UserDetails = data;
+        console.log(data.bids.length);
+        console.log(data.orders.length);
+        console.log(data.ownedApartments.length);
+        const image = data.image;
+        if (image == '') this.userImage = '/assets/imgs/default.jpg';
+        else this.userImage = image;
+        this.spinner.hide('profileSpinner');
 
-    this.spinner.show('imageSpinner')
-    this.myService.Image.subscribe(info=>{
-      this.userImage = info.data.image
-    })
-    this.spinner.hide('imageSpinner')
-    this.spinner.show('profileSpinner')
-    this.myService.GetUserByID(this.userID).subscribe(
-      {
-        next:(data: any)=>{
-          this.UserDetails = data;
-          console.log(data.bids.length)
-          console.log(data.orders.length)
-          console.log(data.ownedApartments.length)
-          const image = data.image
-          if(image == '') this.userImage = "/assets/imgs/default.jpg"
-          else this.userImage = image
-          this.spinner.hide('profileSpinner')
-
-          this.validationForm = new FormGroup({
-            profileImage : new FormControl(this.userImage)
-          })
-        },
-        error:(err: any)=>{console.log(err)}
-      }
-    )
+        this.validationForm = new FormGroup({
+          profileImage: new FormControl(this.userImage),
+        });
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
   }
 
-  onFileSelected(event: any){
-
-    if(this.validateFile(event.target.files[0].name)){
-      this.profileValid = true
-      this.userImage = event.target.files[0]
+  onFileSelected(event: any) {
+    if (this.validateFile(event.target.files[0].name)) {
+      this.profileValid = true;
+      this.userImage = event.target.files[0];
       return;
     }
-
   }
 
   validateFile(name: String) {
     var ext = name.substring(name.lastIndexOf('.') + 1);
     if (ext.toLowerCase() == 'png') {
-        return true;
-    }else if(ext.toLowerCase() == 'jpg'){
-        return true;
-    }
-    else {
-        return false;
+      return true;
+    } else if (ext.toLowerCase() == 'jpg') {
+      return true;
+    } else {
+      return false;
     }
   }
 
-  changeImage(){
-
+  changeImage() {
     const fd = new FormData();
-    fd.append('profileImage' , this.userImage)
-    console.log(fd)
-    this.myService.changeUserPicture(this.userID , fd)
-
+    fd.append('profileImage', this.userImage);
+    console.log(fd);
+    this.myService.changeUserPicture(this.userID, fd).subscribe({
+      next: (response: any) => {
+        this.userImage = response.data['image'];
+      },
+    });
   }
 }
-
