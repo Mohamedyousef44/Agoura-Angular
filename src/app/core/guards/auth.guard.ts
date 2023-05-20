0d54c15e21cf1ff17979fed7e9891e888a8f1d88
mohamed yossef <2020.mohamed.yossef@gmail.com>
2023-05-20 10:15:27 +0300

    add auth guard to toutes that user can not access without log in

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -9,7 +9,12 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      console.log({route , state})
+      if(!localStorage.getItem('X-Auth-Token')){
+        alert('you have to log in first')
+        return false;
+      }
+      return true;
   }
-  
+
 }
