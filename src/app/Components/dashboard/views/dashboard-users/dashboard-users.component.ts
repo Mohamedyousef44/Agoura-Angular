d40a7f103d81e4649dcf6b6d22dfe9da3f5ec2a7
omaralaa <omaralaa0989@gmail.com>
2023-05-24 17:32:03 +0300

    add constructor and ngoninit

--- src/app/Components/dashboard/views/dashboard-users/dashboard-users.component.ts
+++ src/app/Components/dashboard/views/dashboard-users/dashboard-users.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { BidsService } from 'src/app/Service/bids.service';
 
 @Component({
   selector: 'app-dashboard-users',
   templateUrl: './dashboard-users.component.html',
   styleUrls: ['./dashboard-users.component.css']
 })
-export class DashboardUsersComponent {
-  constructor(){
+export class DashboardUsersComponent implements  OnInit{
+allUsers:any
+  constructor(private myService:BidsService,private route:ActivatedRoute,private router:Router){}
+
+  ngOnInit(): void {
 
   }
 
