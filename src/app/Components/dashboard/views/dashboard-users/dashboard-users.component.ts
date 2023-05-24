ff70916f9b9366a91058b9fc1e3f11ebe5c82a70
omaralaa <omaralaa0989@gmail.com>
2023-05-24 17:50:53 +0300

    fix ngoninit

--- src/app/Components/dashboard/views/dashboard-users/dashboard-users.component.ts
+++ src/app/Components/dashboard/views/dashboard-users/dashboard-users.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BidsService } from 'src/app/Service/bids.service';
+import { DashboardTableService } from 'src/app/Service/dashboard-table.service';
 
 @Component({
   selector: 'app-dashboard-users',
   templateUrl: './dashboard-users.component.html',
-  styleUrls: ['./dashboard-users.component.css']
+  styleUrls: ['./dashboard-users.component.css'],
 })
-export class DashboardUsersComponent implements  OnInit{
-allUsers:any
-  constructor(private myService:BidsService,private route:ActivatedRoute,private router:Router){}
+export class DashboardUsersComponent implements OnInit {
+  allUsers: any;
+  constructor(
+    private myService: DashboardTableService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-
+     this.myService.GetAllUsers().subscribe({
+      next: (allData: any) => {
+        console.log(allData.data)
+        this.allUsers = allData.data;
+        console.log(this.allUsers);
+      },
+      error: (err: any) => {
+        console.log(err);
+      },
+    });
   }
-
 }
