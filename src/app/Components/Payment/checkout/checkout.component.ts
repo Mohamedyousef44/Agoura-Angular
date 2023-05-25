60869591133cfe4fe21c7f6f66b437fd832e2700
mahmoud-elbasiony <mahmoud.elbasiony904@gmail.com>
2023-05-25 19:56:25 +0300

    update checkout page

--- src/app/Components/Payment/checkout/checkout.component.ts
+++ src/app/Components/Payment/checkout/checkout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CheckoutService } from 'src/app/Service/checkout.service';
 
 @Component({
@@ -12,20 +12,31 @@ export class CheckoutComponent implements OnInit {
   
   data:any;
   id:any;
-  constructor(private http:CheckoutService,private route:ActivatedRoute){
+
+  constructor(private http:CheckoutService,private checkoutService:CheckoutService,private route:ActivatedRoute,private router:Router){
     this.id=this.route.snapshot.params['id'];
   }
+
   ngOnInit(): void {
     // for the aside info subtotal, taxes and total values 
     console.log(this.id)
     this.http.getOrderDetails(this.id).subscribe({
       next:(res:any)=>{
-        console.log("hamada")
+        console.log(res.data)
         this.data=res.data
-
         console.log(res)
       }
 
+    })
+  }
+  submit(){
+    this.checkoutService.checkout(this.id,this.data).subscribe({
+      next:(res:any)=>{
+        console.log(res)
+        window.location=res.url
+      },
+      error:()=>{
+      }
     })
   }
 
